356bf09c44a1651a32966771cf631a01d3965f2e
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2024-10-11 11:56:15 +0200

    add simple card with motions

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -21,6 +21,16 @@ const Home = () => {
         className="Box bg-teal-500 p-12">
         Box
       </motion.div>
+
+      <motion.div 
+                 initial={{opacity: 0, y:-50}}
+                 animate={{opacity: 1, y: 0}}
+                 duration={{duration: 3.0}}
+                 whileHover={{scale: 1.1}}
+                 whileTap={{scale: 0.9}}
+        className='Card bg-purple-600 text-white p-6 w-[360px] rounded-md'>
+        Card
+      </motion.div>
     </div>
   )
 }
